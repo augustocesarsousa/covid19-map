8f75ec2c98a01797434fcf7d4fbc686acd76882a
Augusto Cesar <acs-03@hotmail.com>
2022-05-10 16:07:01 -0300

    Global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { CssBaseline } from "@material-ui/core";
+import { StylesProvider } from "@material-ui/styles";
+import React from "react";
+import GlobalStyle from "./commons/styles/global-style";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Covid19 map</h1>
-    </div>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <div>test</div>
+    </StylesProvider>
   );
 }
 
